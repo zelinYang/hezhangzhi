12252bda3861bf42dc2d20edfbcfc5f1b6a16d74
YZL <YZL@qq.com>
2019-05-29 02:55:09 +0800

    1

--- src/view/info/info-new.vue
+++ src/view/info/info-new.vue
@@ -12,37 +12,126 @@
 			</el-tag>
 		</div>
 		<div class="left-side-second">
-			<div style="padding-right: 22px">
-				<el-switch
-				style="display: block"
-				v-model="valuesw0"
-				active-text="水质">
-			</el-switch>
-			<el-switch
-				style="display: block"
-				v-model="valuesw1"
-				active-text="污染源">
-			</el-switch>
-		</div>
-			<div style="">
-				<el-switch
-				style="display: block"
-				v-model="valuesw1"
-				active-text="水利工程">
-			</el-switch>
-			<el-switch
-				style="display: block"
-				v-model="valuesw1"
-				active-text="生态流量">
-			</el-switch>
+			<el-tabs type="border-card">
+				<el-tab-pane label="" class="werr">
+					<span slot="label"><i class="el-icon-s-unfold"></i> 选择图层</span>
+					<div style="padding-right: 22px">
+						<el-switch
+						style="display: block"
+						v-model="valuesw0"
+						active-text="水质">
+					</el-switch>
+					<el-switch
+					style="display: block"
+					v-model="valuesw1"
+					active-text="污染源">
+				</el-switch>
 			</div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-		<div class="left-side-third"></div>
-	</div>
-	<div class="right-bottom"></div>
+			<div style="padding-right: 10px"">
+			<el-switch
+			style="display: block"
+			v-model="valuesw1"
+			active-text="水利工程">
+		</el-switch>
+		<el-switch
+		style="display: block"
+		v-model="valuesw1"
+		active-text="生态流量">
+	</el-switch>
+</div>
+<div style="padding-right: 22px">
+	<el-switch
+	style="display: block"
+	v-model="valuesw1"
+	active-text="排污口">
+</el-switch>
+<el-switch
+style="display: block"
+v-model="valuesw1"
+active-text="取水口">
+</el-switch>
+</div>
+<div style="padding-right: 37px">
+	<el-switch
+	style="display: block"
+	v-model="valuesw1"
+	active-text="水位">
+</el-switch>
+<el-switch
+style="display: block"
+v-model="valuesw1"
+active-text="雨情">
+</el-switch>
+</div>
+<div style="padding-right: 22px">
+	<el-switch
+	style="display: block"
+	v-model="valuesw1"
+	active-text="水量">
+</el-switch>
+<el-switch
+style="display: block"
+v-model="valuesw1"
+active-text="公示牌">
+</el-switch>
+</div>
+</el-tab-pane>
+</el-tabs>
+
+</div>
+<div class="left-side-third">
+	<el-tabs type="border-card" style="height: 100%">
+		<el-tab-pane label="" class="werty">
+			<span slot="label"><i class="el-icon-s-unfold"></i> 选择区域</span>
+			<div class="left-side3-item">
+				<span>柳州市</span>
+				<el-radio v-model="radio" label="1"></el-radio>
+			</div>
+			<div>
+				<span>城中区</span>
+				<el-radio v-model="radio" label="2"></el-radio>
+			</div>
+			<div>
+				<span>鱼峰区</span>
+				<el-radio v-model="radio" label="3"></el-radio>
+			</div>
+			<div>
+				<span>柳南区</span>
+				<el-radio v-model="radio" label="4"></el-radio>
+			</div>
+			<div>
+				<span>柳北区</span>
+				<el-radio v-model="radio" label="5"></el-radio>
+			</div>
+			<div>
+				<span>柳江区</span>
+				<el-radio v-model="radio" label="6"></el-radio>
+			</div>
+			<div>
+				<span>鹿寨县</span>
+				<el-radio v-model="radio" label="7"></el-radio>
+			</div>
+			<div>
+				<span>柳城县</span>
+				<el-radio v-model="radio" label="8"></el-radio>
+			</div>
+			<div>
+				<span>融安县</span>
+				<el-radio v-model="radio" label="9"></el-radio>
+			</div>
+			<div>
+				<span>三江侗族自治县</span>
+				<el-radio v-model="radio" label="10"></el-radio>
+			</div>
+			<div>
+				<span>融水苗族自治县</span>
+				<el-radio v-model="radio" label="11"></el-radio>
+			</div>
+		</el-tab-pane>
+	</el-tabs>
+</div>
+</div>
+<div class="right-bottom"></div>
 </div>
 </template>
 
@@ -57,20 +146,49 @@
 				{ name: '关闭预警', type: '' }
 				],
 				valuesw1: true,
-				valuesw0: true
+				valuesw0: true,
+				radio: '1',
 			}
 		}
 
 	})
 </script>
 <style>
-	.el-switch__label{
-		vertical-align: baseline;
+	.el-radio__label{
+		display: none;
 	}
+</style>
+<style>
+.el-switch__label{
+	vertical-align: baseline;
+.el-tabs--border-card{
+	height: 100%;
+}
+}
 </style>
 
 <style scoped lang="less">
+.left-side3-item{
+	padding-left: 10px !important;
+}
 @shadow: 1px 1px 10px #909399;
+.werr>div{
+	display: flex;
+	box-shadow: 0 0 0;
+	min-height: 6.5vh;
+	justify-content: space-between;
+}
+.werr{
+	box-shadow: 0 0 0;
+	height: 100%;
+}
+
+.werty>div{
+	display: flex;
+	justify-content: space-between;
+	padding-left: 30px;
+	height: 3.5vh;
+}
 .left-side{
 	max-width: 280px;
 	min-width: 280px;
@@ -78,10 +196,11 @@
 	position: fixed;
 	left: 10px;
 	top: 70px;
-	div{
-		box-shadow: @shadow;
-		margin-bottom: 10px;
-	}
+	
+}
+.left-side>div{
+	box-shadow: @shadow;
+	margin-bottom: 10px;
 }
 .left-side-first{
 	height: 50px;
@@ -91,20 +210,12 @@
 .left-side-second{
 	min-height: 300px;
 	height: 40vh;
-	div{
-		height: 3vh;
-		display: flex;
-		line-height: 3vh;
-		justify-content: space-between;
-		box-shadow: 0 0 0 #efefef;
-		padding: 10px;
-		span.el-switch__label{
-			vertical-align: baseline;
-		}
-	}
 }
 .left-side-third{
 	min-height: 400px;
 	height: 45vh;
+}
+.left-side-third>div{
+	
 }
 </style>
